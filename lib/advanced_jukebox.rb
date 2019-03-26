aad73abb9ca7e9d1bb10627a737381225ecd6cb9
Rob O'Gara <rgo594@gmail.com>
2019-03-26 16:51:26 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,45 +1,65 @@
-#Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
-#make sure to edit the value of each key to replace < path to this directory >
-#with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
-
-def help
-  #this method should be the same as in jukebox.rb
-
+def help()
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
-
-
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+  my_songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
 end
 
 
-def play(my_songs)
-  #this method is slightly different!
-  #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
-  #check to see if the name they give is in fact a key of the my_songs hash
-  #if it isn't, tell them their choice is invalid
-  #if it is, play the song using the system 'open <file path>' syntax
-  #get the file path of the song by looking it up in the my_songs hash
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp()
   
+  if (1..9).to_a.index(input.to_i) != nil
+    puts "Playing #{songs[input.to_i - 1]}"
+  elsif my_songs.index(input) != nil
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
-def exit_jukebox
-  #this method is the same as in jukebox.rb
+def exit_jukebox()
+  puts "Goodbye"
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
+def prompt()
+  puts "Please enter a command:"
+  gets.chomp()
 end
+
+  
+def run(songs)
+  help()
+  input = prompt()
+  
+  while input != "exit"
+    if input == "list"
+      list(songs)
+      input = prompt()
+    elsif input == "play"
+      play(songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
+    end
+  end
+  
+  exit_jukebox()
+end
+
+
+
