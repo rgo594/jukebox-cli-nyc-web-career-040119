514172ad1b8660cc0b88e62d22976b7b3e5ab3b1
Rob O'Gara <rgo594@gmail.com>
2019-03-26 16:50:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,11 +1,3 @@
-# def say_hello(name)
-#   "Hi #{name}!"
-# end
- 
-# puts "Enter your name:"
-# users_name = gets.chomp
- 
-# puts say_hello(users_name)
 
 songs = [
   "Phoenix - 1901",
@@ -19,15 +11,6 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-# THE HELP METHOD
-# This method should puts out the following:
-
-  # I accept the following commands:
-  # - help : displays this help message
-  # - list : displays a list of songs you can play
-  # - play : lets you choose a song to play
-  # - exit : exits this program
-  
 def help()
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -36,33 +19,11 @@ def help()
   puts "- exit : exits this program"
 end
 
-#help
-
-# THE LIST METHOD
-# This method takes in an argument of the songs array of puts out the following:
-
-  # 1. Phoenix - 1901
-  # 2. Tokyo Police Club - Wait Up
-  # 3. Sufjan Stevens - Too Much
-  # 4. The Naked and the Famous - Young Blood
-  # 5. (Far From) Home - Tiga
-  # 6. The Cults - Abducted
-  # 7. Phoenix - Consolation Prizes
-  # 8. Harry Chapin - Cats in the Cradle
-  # 9. Amos Lee - Keep It Loose, Keep It Tight
-
 def list(my_songs)
   my_songs.each_with_index do |song, index|
     puts "#{index+1}. #{song}"
   end
 end
-
-#list(songs)
-
-# THE PLAY METHOD
-# This method also takes in an argument of the songs array. It first puts out the prompt: "Please enter a song name or number:". It then stores the user's response using gets.chomp.
-
-# If the user's response is a valid song number or song name, the method should puts out: "Playing <song name>". Otherwise, it should puts out: "Invalid input, please try again".
 
 def play(my_songs)
   puts "Please enter a song name or number:"
