7d770cbf06d474fc27e5ed955f69fa3b764a21da
Rob O'Gara <rgo594@gmail.com>
2019-03-26 16:46:22 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,45 +1,136 @@
-#Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
-#make sure to edit the value of each key to replace < path to this directory >
-#with the correct path to this directory on your computer
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+ 
+# puts "Enter your name:"
+# users_name = gets.chomp
+ 
+# puts say_hello(users_name)
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
 
-def help
-  #this method should be the same as in jukebox.rb
+# THE HELP METHOD
+# This method should puts out the following:
 
+  # I accept the following commands:
+  # - help : displays this help message
+  # - list : displays a list of songs you can play
+  # - play : lets you choose a song to play
+  # - exit : exits this program
+  
+def help()
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
+#help
 
+# THE LIST METHOD
+# This method takes in an argument of the songs array of puts out the following:
+
+  # 1. Phoenix - 1901
+  # 2. Tokyo Police Club - Wait Up
+  # 3. Sufjan Stevens - Too Much
+  # 4. The Naked and the Famous - Young Blood
+  # 5. (Far From) Home - Tiga
+  # 6. The Cults - Abducted
+  # 7. Phoenix - Consolation Prizes
+  # 8. Harry Chapin - Cats in the Cradle
+  # 9. Amos Lee - Keep It Loose, Keep It Tight
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+  my_songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
 end
 
+#list(songs)
 
-def play(my_songs)
-  #this method is slightly different!
-  #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
-  #check to see if the name they give is in fact a key of the my_songs hash
-  #if it isn't, tell them their choice is invalid
-  #if it is, play the song using the system 'open <file path>' syntax
-  #get the file path of the song by looking it up in the my_songs hash
+# THE PLAY METHOD
+# This method also takes in an argument of the songs array. It first puts out the prompt: "Please enter a song name or number:". It then stores the user's response using gets.chomp.
+
+# If the user's response is a valid song number or song name, the method should puts out: "Playing <song name>". Otherwise, it should puts out: "Invalid input, please try again".
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp()
   
+  if (1..9).to_a.index(input.to_i) != nil
+    puts "Playing #{songs[input.to_i - 1]}"
+  elsif my_songs.index(input) != nil
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
-def exit_jukebox
-  #this method is the same as in jukebox.rb
+def exit_jukebox()
+  puts "Goodbye"
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
+def prompt()
+  puts "Please enter a command:"
+  gets.chomp()
 end
+
+# THE RUN METHOD
+# This method is a little trickier. It will use the other methods we built, our "helper" methods, to actually enact the running of our Jukebox.
+
+# First, this method should call on the help method to show the user the available commands. Then, it should puts out the prompt: "Please enter a command:". It should capture the user's response using gets.chomp or gets.strip.
+
+# We need to keep our program running as long as the user's input is not "exit". Use a loop to continue asking the user for input until or unless their input is "exit". Use if or case statements to determine how your program will respond to a user's input. For example, if their input is "list", call the list method, if their input is "play", call the play method, if their input is "help", call the help method and if their input is "exit", call the exit_jukebox method and break out of your loop to stop the program.
+  
+def run(songs)
+  help()
+  input = prompt()
+  
+  while input != "exit"
+    if input == "list"
+      list(songs)
+      input = prompt()
+    elsif input == "play"
+      play(songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
+    end
+  end
+  
+  exit_jukebox()
+end
+
+#run(songs)
+
+# def annoying_program()
+#   puts "What is your name?: "
+#   input = gets.chomp()
+  
+#   while input != "ahamed"
+#     puts "What is your name?: "
+#     input = gets.chomp()
+#   end 
+  
+#   "Goodday #{input}!"
+# end
+
+# annoying_program()
+
+
